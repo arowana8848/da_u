220a0daa366cb996e7bbba8add0f071865e2406b
= <=>
2025-02-25 15:30:40 +0545

    updated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import Home from "./components/Home"
 import About from "./components/About"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
-import AdminLogin from "./components/AdminLogin"
-import AdminDashboard from "./components/AdminDashboard"
-import Staff from "./components/Staff"
 import ExpenseTracker from "./components/ExpenseTracker"
 import GoalSetup from "./components/GoalSetup";
 import IDStorage from "./components/IDStorage";
@@ -16,15 +13,12 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/" element={<Navigate to="/login"/>} />
 
           <Route path="/home" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
-          <Route path="/admin-login" element={<AdminLogin/>}/>
-          <Route path="/dashboard" element={<AdminDashboard/>}/>
-          <Route path="/staff" element={<Staff/>}/>
 
           <Route path="/expense-tracker" element={<ExpenseTracker/>} />
           <Route path="/goal-setup" element={<GoalSetup/>} />
